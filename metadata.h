76ae94d18c166c438de6d3991cd6d370c2a7fb8e
blankworker <blankworks@proton.me>
2026-03-14 18:26:14 +0100

    Add files via upload

---
+++ metadata.h
@@ -0,0 +1,5 @@
+{
+  "name": "Sats Vouchers",
+  "description": "A premium Bitcoin voucher management application for generating, printing, and tracking BTC vouchers.",
+  "requestFramePermissions": []
+}
